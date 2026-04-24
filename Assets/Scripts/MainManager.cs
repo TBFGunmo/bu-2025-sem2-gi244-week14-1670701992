036bed08f1cae4958d371d5163426cb712c2eaf3
TBFGunmo <gunmohome123@gmail.com>
2026-04-24 10:46:33 +0700

    last

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -60,10 +60,15 @@ public class MainManager : MonoBehaviour
         string fileName = "saveData.json";
         string fullPath = Path.Combine(p, fileName);
 
-        string j = File.ReadAllText(fullPath);
-        Debug.Log(j);
-        SaveData data = JsonUtility.FromJson<SaveData>(j);
-        TeamColor = data.TeamColor;
+        if (File.Exists(fullPath)) 
+        {
+            string j = File.ReadAllText(fullPath);
+            Debug.Log(j);
+            SaveData data = JsonUtility.FromJson<SaveData>(j);
+            TeamColor = data.TeamColor;
+        }
+
+        
 
 
         /*string j = PlayerPrefs.GetString("saveData");
